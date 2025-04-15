9bb98b8e60b6776e11cb4c3da0de585dada3b71f
Jeremy <jeremylm21@gmail.com>
2025-04-14 20:16:55 -0600

    idk anymore

--- stock-generator/stock_generator.py
+++ stock-generator/stock_generator.py
@@ -12,7 +12,7 @@ SLEEP_SECONDS = 1
 
 # Set up Kafka producer
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
+    bootstrap_servers="localhost:29092",
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
